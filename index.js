3b5db2bd93b47fd149fa77f048b99fe48a67a417
Matthew Henderson <info@matthewahenderson.com>
2022-06-19 16:52:56 -0400

    Refactored to use await and Promise.all; returns name of the file using new approach and returns true or false to tell if it is a file or not

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 #!/usr/bin/env node
 
+// Create fs object and use module to get lstat
+const { all } = require('express/lib/application.js');
 const fs = require('fs');
+const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), (err, filenames) => {
+// Use fs object to get file or folder
+fs.readdir(process.cwd(), async (err, filenames) => {
     if (err) {
         console.log(err); 
     }
 
-    console.log(filenames);
+    const statPromises = filenames.map(filename => {
+        return lstat(filename);
+    })
+
+    const allStats = await Promise.all(statPromises);
+
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats);
+
+        console.log(filenames[index], stats.isFile());
+    }
 });
